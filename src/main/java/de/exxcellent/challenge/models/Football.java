ec2e66757daa215c0c5a60800968d33461f3b107
Tuan Nguyen <56967422+tbtuan@users.noreply.github.com>
2023-08-11 15:54:32 +0200

    add Football model

---
+++ src/main/java/de/exxcellent/challenge/models/Football.java
@@ -0,0 +1,40 @@
+package de.exxcellent.challenge.models;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class Football {
+    @CsvBindByName(column = "Team", required = true)
+    private String team;
+    @CsvBindByName(column = "Games", required = true)
+    private int goals;
+    @CsvBindByName(column = "Goals Allowed", required = true)
+    private int goalsAllowed;
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public void setGoalsAllowed(int goalsAllowed) {
+        this.goalsAllowed = goalsAllowed;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public int getGoalsAllowed() {
+        return goalsAllowed;
+    }
+
+    public int getGoalSpread() {
+        return Math.abs(goals - goalsAllowed);
+    }
+}
